054ff70e78a3520af3590188a74c3b2cddfdf2b9
Slaaf <Antonio.Slaaf@hologic.com>
2018-04-30 17:37:46 -0700

    async

--- test/tx.test.js
+++ test/tx.test.js
@@ -7,13 +7,11 @@ var Readable = require('stream').Readable
 
  describe('TxTest', function() {
 	 
-	 //TODO Find out why Need to add extra 00
  	rawTx = Buffer.from('0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600','hex')
 	rawTx2 = Buffer.from('76a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac','hex') 
 	readable = new Readable()
 	readable.push(rawTx)
 	readable.push(null)
-	//readable.setEncoding('UTF8')
 		
 	tx = new Tx.Tx(readable)
 	sc = new Script.Script(rawTx2);
@@ -47,6 +45,7 @@ var Readable = require('stream').Readable
 	
 	it('test parse locktime', function() {
 		assert.deepEqual(tx.locktime, 410393);
+
 	})
 	
 	it('test der signature', function() {
@@ -55,6 +54,7 @@ var Readable = require('stream').Readable
 		hashType = tx.inputs[0].hashType()
 		assert.equal(der.toString('hex'), want)
 		assert.equal(hashType, SIGHASH_ALL)
+
 	})
 	
 	it('test sec pubkey', function() {
@@ -63,7 +63,27 @@ var Readable = require('stream').Readable
 	})
 	
 	it('test serialize', function() {
-		assert.equal(tx.serialize(), rawTx)
+
+		assert.deepEqual(tx.serialize(), rawTx)
+
+	})
+	
+	it('test input value', function() {
+
+		txHash = 'd1c789a9c60383bf715f3f6ad9d14b91fe55f3deb369fe5d9280cb1a01793f81'
+		index = 0
+		want = 42505594
+		txIn = tx.inputs[0]
+		//console.log('tiv0', tx.inputs[0].value())
+		tx.inputs[0].value().then(data => {
+			console.log('data', data)
+			assert.equal(data, want) 
+			//done()
+
+		})
+		console.log('tiv1')
+		//done()
+		
 	})
 	 
  })
--- tx.js
+++ tx.js
@@ -1,6 +1,8 @@
-
+//var request = require('request')
+var https = require('https')
 var helper = require('./helper')
 var script = require('./script')
+var Readable = require('stream').Readable
 
 class Tx {
 	constructor(s) {
@@ -23,19 +25,23 @@ class Tx {
 		this.locktime = helper.littleEndianToInt(s.read(4))
 
 	}	
+	
+	static parse(s) {
+		
+	}
 
 	serialize() {
 		result = helper.intToLittleEndian(this.version, 4)
 		result = Buffer.concat([result , helper.encodeVarint(this.inputs.length)])
 		this.inputs.map( obj => {
 			result = Buffer.concat([result , obj.serialize()])
+			
 		})
 		result = Buffer.concat([result , helper.encodeVarint(this.outputs.length)])
 		this.outputs.map ( obj => {
 			result = Buffer.concat([result, obj.serialize()]);
 		})
 		result = Buffer.concat([result, helper.intToLittleEndian(this.locktime, 4)]);
-		console.log(result.toString('hex'))
 		return result;
 	}
 }
@@ -48,20 +54,65 @@ class TxIn {
 		this.scriptSig = new script.Script(s.read(scriptSigLength))
 		const x = s.read(4)
 		this.sequence = helper.littleEndianToInt(x)
+		this.cache = {}
 		
 	}
 
 	serialize() {
-		let result = Array.prototype.reverse.call(this.prevTx)
+		const ta = Buffer.from(this.prevTx);
+		let result = Array.prototype.reverse.call(ta)
 		result = Buffer.concat([result , helper.intToLittleEndian(this.prevIndex, 4)]);
 		const rawScriptSig = this.scriptSig.serialize();
-		result = Buffer.concat([result, rawScriptSig]);
 		result = Buffer.concat([result, helper.encodeVarint(rawScriptSig.length)])
+		result = Buffer.concat([result, rawScriptSig]);
 		result = Buffer.concat([result, helper.intToLittleEndian(this.sequence,4)])
-		console.log('txin', result)
 		return result;
 	}
 	
+	getUrl(testnet = false) {
+		return (testnet ?  'https://testnet.blockexplorer.com/api' :  'https://btc-bitcore3.trezor.io/api');
+	}
+	
+	fetchTx(testnet=false) {
+		console.log('fetch')
+		if (!(this.prevTx in this.cache)) {
+			const url = this.getUrl(testnet) + '/rawtx/' + this.prevTx.toString('hex');
+			console.log(url)
+			return new Promise((resolve, reject) => {
+				https.get(url, function(res) { 
+					if (res.statusCode < 200 || res.statusCode > 299) {
+						reject(new Error('Failed to load page, status code: ' + response.statusCode));
+					}
+				
+					//return JSON.parse(body).rawtx;
+					const body = []
+					res.on('data', (chunk) => body.push(chunk));
+					// we are done, resolve promise with those joined chunks
+					res.on('end', () => resolve(body.join('')));			
+				});
+			})
+		}
+	}
+	
+	value(testnet=false) {
+		
+		return this.fetchTx(testnet=testnet)
+			.then( (html) => {
+				console.log(html)
+				const raw = Buffer.from(JSON.parse(html).rawtx)
+				readable = new Readable()
+				readable.push(rawTx)
+				readable.push(null)
+				tx = new Tx(readable);
+				console.log(tx)
+				console.log('txa',tx.outputs[this.prevIndex].amount)
+				//return 42505594;
+				return tx.outputs[this.prevIndex].amount;
+
+			}).catch((err) => { return -9999});
+			
+		//return tx.ouputs[this.prevIndex].amount;
+	}
 	derSignature(index=0) {
 		const signature = this.scriptSig.signature(index=index)
 		return signature.slice(0,signature.length-1);
@@ -85,11 +136,10 @@ class TxOut {
 	}
 
 	serialize() {
-		let result = helper.intToLittleEndian(this.amout, 8);
+		let result = helper.intToLittleEndian(this.amount, 8);
 		const rawScriptPubkey = this.scriptPubkey.serialize();
 		result = Buffer.concat([result, helper.encodeVarint(rawScriptPubkey.length)]);
 		result = Buffer.concat([result, rawScriptPubkey]);
-		console.log('txout', result)
 		return result;
 	}
 }
